15c88b4b693a229103772bd5b8b17ef66be5159f
KhachaneMayur <mkhachane1000@gmail.com>
2022-07-14 19:03:01 +0530

    Update hello world file.

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
+		  System.out.println("This is the local changes that need to be tested.");
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
